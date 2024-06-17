ab29e02310d6068634578e065d3893049eaac084
Akash01997 <akashshinde558@gmail.com>
2024-06-17 16:13:10 +0530

    Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import { useState } from "react"
+import Data from "./Data.js"
+import ProductCard from "./ProductCard.jsx"
 
 const Home = () => {
+
+  const [productData, setProductData] = useState([...Data])
+  
+  let handleRating =()=>{
+    let filteredRatingData = productData.filter((obj)=>{
+      return obj.rating > 4
+    });
+    setProductData(filteredRatingData)
+  }
+
+  let handleCategory =(category)=>{
+    let filteredfurniture = productData.filter((obj)=>{
+      return obj.category == category
+    });
+    setProductData(filteredfurniture)
+  }
   return (
-    <div className='border-black'>
-      This is Home
-    </div>
+      <div className="h-[91vh] w-screen flex flex-col">
+
+      <div className="utility flex w-100 justify-around mt-2"> 
+      <button className="btn btn-primary"onClick={handleRating}>Top-Rating</button>
+      <button className="btn btn-primary"onClick={()=>handleCategory("furniture")}>Furniture</button>
+      
+      <div className="searchbar">
+        <input type="text" placeholder="Search" className="h-9 rounded-2xl"></input>
+        <button className="btn btn-success mx-2">Search</button>
+      </div>
+      <button className="btn btn-primary" onClick={()=>handleCategory("beauty")}>Beauty</button>
+      <button className="btn btn-primary" onClick={()=>handleCategory("groceries")}>Grocery</button>
+      </div>
+      <div className="flex flex-wrap justify-around">
+      {
+        productData.map((obj)=>{
+          return <ProductCard obj={obj}></ProductCard>
+        })
+      }
+      </div>
+      
+      </div>
+   
   )
 }
 
 export default Home
+                                   
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,22 @@
+let ProductCard = ({obj})=>{
+
+  let {title, thumbnail, rating, price, category} = obj
+
+  return(
+        <div className="card w-96 bg-base-300 shadow-xl m-3">
+        <figure><img src={thumbnail} alt="Shoes" /></figure>
+            <div className="card-body">
+             <h2 className="card-title">{title}</h2>
+                <div className="card-actions justify-between items-center">
+                  <div className="badge badge-secondary bg-green-300">Category: {category}</div>
+                  <div className="badge badge-secondary bg-green-300">Rating: {rating}</div>
+                </div>
+                <p className="p-1">$ {price} </p>
+                <button class="bg-black text-white p-4 rounded-2xl">Add</button>
+            </div>
+        </div>
+    );
+};
+
+
+export default ProductCard;
