0fe14c36b6b050959163f626a8902c0f86781ca3
Akash01997 <akashshinde558@gmail.com>
2024-06-20 13:36:19 +0530

    Product page render

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,18 @@
+import { useNavigate } from "react-router-dom";
 
 let ProductCard = ({obj})=>{
 
-  let {title, thumbnail, rating, price, category} = obj
+  let {title, thumbnail, rating, price, category, id} = obj
+  let Navigate = useNavigate();
 
+  let handleClick= ()=>{
+  Navigate(`/product/${id}`)
+  }
+  let handleAddbtn=(event)=>{
+    event.stopPropagation()
+  }
   return(
-        <div className="card w-96 bg-base-300 shadow-xl m-3">
+        <div className="card w-96 bg-base-300 shadow-xl m-3" onClick={handleClick}>
         <figure><img src={thumbnail} alt="Shoes" /></figure>
             <div className="card-body">
              <h2 className="card-title">{title}</h2>
@@ -12,8 +20,8 @@ let ProductCard = ({obj})=>{
                   <div className="badge badge-secondary bg-green-300">Category: {category}</div>
                   <div className="badge badge-secondary bg-green-300">Rating: {rating}</div>
                 </div>
-                <p className="p-1">$ {price} </p>
-                <button class="bg-black text-white p-4 rounded-2xl">Add</button>
+                <p className="p-1">${price} </p>
+                <button class="bg-black text-white p-4 rounded-2xl" onClick={handleAddbtn}>Add</button>
             </div>
         </div>
     );
