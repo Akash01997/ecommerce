d788b7a7e6a0d63898734418bcac3ca44e82749e
Akash01997 <akashshinde558@gmail.com>
2024-06-20 11:19:56 +0530

    Router

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      I am Card
+    </div>
+  )
+}
+
+export default Card
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import About from './components/About.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+let Router = createBrowserRouter([
+  {
+   path : "/",
+   element : <App></App>
+  },
+  {
+    path : "/about",
+    element : <About></About>
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
