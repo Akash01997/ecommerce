a2da82d43581310cc117e6917024e4a660162c50
Akash01997 <akashshinde558@gmail.com>
2024-06-27 10:05:20 +0530

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,81 +1,96 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-import ProductCard from "./ProductCard.jsx"
-import ShimmerUI from "./ShimmerUI.jsx"
+import ProductCard from "./ProductCard.jsx";
+import ShimmerUI from "./ShimmerUI.jsx";
 
 const Home = () => {
+  const [productData, setProductData] = useState([]);
+  const [allProductData, setAllProductData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const [productData, setProductData] = useState([])
-  const [allProductData, setAllProductData] = useState([])
-  const [searchQuery, setSearchQuery] = useState("")
+  let getData = async () => {
+    let data = await fetch("http://dummyjson.com/products");
+    let obj = await data.json();
+    setAllProductData(obj.products);
+    setProductData(obj.products);
+  };
 
+  useEffect(() => {
+    getData();
+  }, []);
 
- let getData = async()=>{
-  let data = await fetch("http://dummyjson.com/products")
-  let obj = await data.json();
-  setAllProductData(obj.products)
-  setProductData(obj.products)
- }
-
- useEffect(()=>{
-  getData()
- },[])
-
-
- if(allProductData.length ==0){
-  return <ShimmerUI></ShimmerUI>
- }
-  let handleRating =()=>{
-    let filteredRatingData = allProductData.filter((obj)=>{
-      return obj.rating > 4
-    });
-    setProductData(filteredRatingData)
+  if (allProductData.length == 0) {
+    return <ShimmerUI></ShimmerUI>;
   }
-
-  let handleCategory =(category)=>{
-    let filteredfurniture = allProductData.filter((obj)=>{
-      return obj.category == category
+  let handleRating = () => {
+    let filteredRatingData = allProductData.filter((obj) => {
+      return obj.rating > 4;
     });
-    setProductData(filteredfurniture)
-  }
+    setProductData(filteredRatingData);
+  };
 
-  let handleSearch=()=>{
-    let filteredData = allProductData.filter((obj)=>{
-      return obj.title.toLowerCase().includes(searchQuery.toLowerCase().trim)
+  let handleCategory = (category) => {
+    let filteredfurniture = allProductData.filter((obj) => {
+      return obj.category == category;
     });
-    setProductData(filteredData)
-    setSearchQuery("")
-  }
+    setProductData(filteredfurniture);
+  };
 
- 
+  let handleSearch = () => {
+    let filteredData = allProductData.filter((obj) => {
+      return obj.title.toLowerCase().includes(searchQuery.toLowerCase().trim);
+    });
+    setProductData(filteredData);
+    setSearchQuery("");
+  };
+
   return (
-      <div className="h-[91vh] w-screen flex flex-col">
+    <div className="h-[91vh] w-screen flex flex-col">
+      <div className="utility flex w-100 justify-around mt-2">
+        <button className="btn btn-primary" onClick={handleRating}>
+          Top-Rating
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => handleCategory("furniture")}
+        >
+          Furniture
+        </button>
 
-      <div className="utility flex w-100 justify-around mt-2"> 
-      <button className="btn btn-primary"onClick={handleRating}>Top-Rating</button>
-      <button className="btn btn-primary"onClick={()=>handleCategory("furniture")}>Furniture</button>
-      
-      <div className="searchbar">
-        <input type="text" placeholder="Search" className="h-9 rounded-2xl" value={searchQuery} onChange={(event)=>{
-        setSearchQuery(event.target.value)
-        }}></input>
-        <button className="btn btn-success mx-2" onClick={handleSearch}>Search</button>
-      </div>
-      <button className="btn btn-primary" onClick={()=>handleCategory("beauty")}>Beauty</button>
-      <button className="btn btn-primary" onClick={()=>handleCategory("groceries")}>Grocery</button>
+        <div className="searchbar">
+          <input
+            type="text"
+            placeholder="Search"
+            className="h-9 rounded-2xl"
+            value={searchQuery}
+            onChange={(event) => {
+              setSearchQuery(event.target.value);
+            }}
+          ></input>
+          <button className="btn btn-success mx-2" onClick={handleSearch}>
+            Search
+          </button>
+        </div>
+        <button
+          className="btn btn-primary"
+          onClick={() => handleCategory("beauty")}
+        >
+          Beauty
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => handleCategory("groceries")}
+        >
+          Grocery
+        </button>
       </div>
       <div className="flex flex-wrap justify-around">
-      {
-        productData.map((obj)=>{
-          return <ProductCard obj={obj}></ProductCard>
-        })
-      }
+        {productData.map((obj) => {
+          return <ProductCard obj={obj}></ProductCard>;
+        })}
       </div>
-      
-      </div>
-   
-  )
-}
+    </div>
+  );
+};
 
-export default Home
-                                   
+export default Home;
